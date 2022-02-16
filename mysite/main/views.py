ad83a35fedc86fbf339a015c54fafe8fa2ee1263
drboss06 <s4sha228@yandex.ru>
2022-02-16 21:29:35 +0700

    last_learn

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -6,5 +6,4 @@ def profile(request):
     latest_memories_list = Memories.objects.order_by('-date_memorie')
     context = {'latest_memories_list': latest_memories_list}
 
-
     return render(request, 'main/profile.html', context)
